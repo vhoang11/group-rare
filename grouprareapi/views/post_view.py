a1cfa0685da2a64ce8d88752d24af1ce7b2cbbf1
GrahamJReid <grahamreidtechnology@gmail.com>
2023-06-19 11:10:20 -0500

    CRUD on Posts functional

--- grouprareapi/views/post_view.py
+++ grouprareapi/views/post_view.py
@@ -16,11 +16,11 @@ class PostView(ViewSet):
             Response -- JSON serialized game type
         """
         try:
-            game_type = Event.objects.get(pk=pk)
-            serializer = EventSerializer(game_type)
+            post = Post.objects.get(pk=pk)
+            serializer = PostSerializer(post)
             return Response(serializer.data)
-        except Event.DoesNotExist as ex:
-          return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        except Post.DoesNotExist:
+          return Response({'message': 'Post does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
 
     def list(self, request):
@@ -29,29 +29,15 @@ class PostView(ViewSet):
         Returns:
             Response -- JSON serialized list of game types
         """
-        events = Event.objects.all()
-        
-        game_type = request.query_params.get('game', None)
-        if game_type is not None:
-          events = events.filter(game_id=game_type)
-        
-        uid = request.META['HTTP_AUTHORIZATION']
-        gamer = Gamer.objects.get(uid=uid)
-
-        for event in events:
-            # Check to see if there is a row in the Event Games table that has the passed in gamer and event
-            event.joined = len(EventGamer.objects.filter(
-                gamer=gamer, event=event)) > 0
-  
-          
-        serializer = EventSerializer(events, many=True)
-        return Response(serializer.data)
+        post = Post.objects.all()
+        serializer = PostSerializer(post, many=True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
     
     def create(self, request):
  
        
-        rUserId = RareUser.objects.get(pk=request.data["rare_user_id"])
-        catId = Category.objects.get(pk=request.data["category_id"])
+        rUserId = RareUser.objects.get(pk=request.data["rareUserId"])
+        catId = Category.objects.get(pk=request.data["categoryId"])
 
         post = Post.objects.create(
             title=request.data["title"],
@@ -68,47 +54,27 @@ class PostView(ViewSet):
     
     def update(self, request, pk):
 
-        event = Event.objects.get(pk=pk)
-        event.game = Game.objects.get(pk=request.data["game"])
-        event.description = request.data["description"]
-        event.date = request.data["date"]
-        event.time = request.data["time"]
+        post = Post.objects.get(pk=pk)
+        post.title = request.data["title"]
+        post.publication_date = request.data["publicationDate"]
+        post.image_url=request.data["imageUrl"]
+        post.content=request.data["content"]
+        post.approved=request.data["approved"]
+        category_id= Category.objects.get(pk=request.data["categoryId"])
+        rare_user_id = RareUser.objects.get(pk=request.data["rareUserId"])
+        
+       
 
-        organizer= Gamer.objects.get(uid=request.data["userId"])
-
-        event.save()
+        post.save()
 
         return Response(None, status=status.HTTP_204_NO_CONTENT)   
     
     def destroy(self, request, pk):
-        event = Event.objects.get(pk=pk)
-        event.delete()
+        post = Post.objects.get(pk=pk)
+        post.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
     
-    @action(methods=['post'], detail=True)
-    def signup(self, request, pk):
-        """Post request for a user to sign up for an event"""
-
-        gamer = Gamer.objects.get(uid=request.META['HTTP_AUTHORIZATION'])
-        event = Event.objects.get(pk=pk)
-        attendee = EventGamer.objects.create(
-            gamer=gamer,
-            event=event
-        )
-        return Response({'message': 'Gamer added'}, status=status.HTTP_201_CREATED)
     
-    @action(methods=['delete'], detail=True)
-    def leave(self, request, pk):
-        """DELETE request for a user to sign up for an event"""
-
-        gamer = Gamer.objects.get(uid=request.META['HTTP_AUTHORIZATION'])
-        event = Event.objects.get(pk=pk)
-        attendee = EventGamer.objects.get(
-            gamer=gamer,
-            event=event
-        )
-        attendee.delete()
-        return Response({'message': 'Gamer removed'}, status=status.HTTP_204_NO_CONTENT)
 class PostSerializer(serializers.ModelSerializer):
     """JSON serializer for events
     """
