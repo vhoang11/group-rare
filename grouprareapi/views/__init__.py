33b7f62c783e75ba203db0a8af81065ee29ca528
Van Hoang <111828642+vhoang11@users.noreply.github.com>
2023-06-19 21:04:53 -0500

    users view added

--- grouprareapi/views/__init__.py
+++ grouprareapi/views/__init__.py
@@ -2,3 +2,4 @@ from .auth import check_user, register_user
 from .comment import CommentView
 from .post_view import PostView
 from .category import CategoryView
+from .rare_user import RareUserView
