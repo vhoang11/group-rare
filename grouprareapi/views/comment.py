5811ce784594a1cad39e76a3c103e2d65f60d166
Charles Bridgers IV (he/him) <charlesbridgersiv@gmail.com>
2023-06-20 20:25:38 -0500

    comments display by post id

--- grouprareapi/views/comment.py
+++ grouprareapi/views/comment.py
@@ -5,6 +5,7 @@ from rest_framework.serializers import ModelSerializer
 from django.db.models import Count, Q
 from django.core.exceptions import ValidationError
 from grouprareapi.models import Comment, RareUser, Post
+from datetime import datetime
 
 class CommentView(ViewSet):
     """Comment view set"""
@@ -13,11 +14,12 @@ class CommentView(ViewSet):
         """
         author_id = RareUser.objects.get(pk=request.data["author_id"])
         post_id = Post.objects.get(pk=request.data["post_id"])
+        created_on = datetime.strptime(request.data["created_on"], "%B %d, %Y, %I:%M%p")
         comment = Comment.objects.create(
             author_id=author_id,
             post_id=post_id,
             content=request.data["content"],
-            created_on=request.data["created_on"],
+            created_on=created_on,
         )
         serializer = CommentSerializer(comment)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -30,7 +32,8 @@ class CommentView(ViewSet):
     
     def list(self, request):
         """GET All Comments"""
-        comments = Comment.objects.all()
+        post_id = request.query_params.get('postId', None)
+        comments = Comment.objects.all().filter(post_id=post_id)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
@@ -40,7 +43,7 @@ class CommentView(ViewSet):
         comment.author_id = RareUser.objects.get(pk=request.data["author_id"])
         comment.post_id_id = Post.objects.get(pk=request.data["post_id"])
         comment.content = request.data["content"]
-        comment.created_on = request.data["createdOn"]
+        comment.created_on = request.data["created_on"]
         comment.save()
         return Response('Comment edited', status=status.HTTP_200_OK)
     
@@ -50,9 +53,9 @@ class CommentView(ViewSet):
         comment.delete()
         return Response('Comment deleted', status=status.HTTP_204_NO_CONTENT)
     
-        
 class CommentSerializer(serializers.ModelSerializer):
     """JSON Serializer for comments"""
+    created_on = serializers.DateTimeField(format="%B %d, %Y, %I:%M%p")
     class Meta:
         model = Comment
         fields = ('id', 'author_id', 'post_id', 'content', 'created_on')
